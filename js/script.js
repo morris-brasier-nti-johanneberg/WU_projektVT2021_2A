5a573e2d63ca562e9b1c2b306bb560561ba1d638
morris-brasier-nti-johanneberg <morris.brasier@elev.ga.ntig.se>
2021-02-26 23:30:26 +0100

    Hamburger meny skelett klart

---
+++ js/script.js
@@ -0,0 +1,9 @@
+const nav = document.querySelector('.navLinks')
+const burger = document.querySelector('.burger')
+
+function toggleMeny() {
+    nav.classList.toggle('navActive')
+    burger.classList.toggle('toggle')
+}
+
+burger.addEventListener('click', toggleMeny)
